d66b78d85ae722fc240c01a872b9ea6843f45829
Daniel Konhauser <dkonhauser@gmail.com>
2021-01-02 19:33:14 -0800

    Update index.js
    forward and backward edit of DS track

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var soundSource = 'https://res.cloudinary.com/de3c6e2g5/video/upload/v1609633428/2-31_57B__Same_With_Reverb_jepajc.wav';
+var soundSource = 'https://res.cloudinary.com/de3c6e2g5/video/upload/v1609644729/EVP_fwd_bk_iw0h0c.wav';
 
 var audioContext = new AudioContext()
 
